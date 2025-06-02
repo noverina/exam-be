aa9997d6fd60a6ab82254078d7b6178727354eab
Noverina Sari <noverina.sari@indocyber.id>
2025-06-02 17:04:32 +0700

    :recycle: adjusted the corresponding files to adhere to using uuid :sparkles: added endpoint for fetching exam grade detail

--- src/main/java/porto/exam/controllers/CourseController.java
+++ src/main/java/porto/exam/controllers/CourseController.java
@@ -1,6 +1,7 @@
 package porto.exam.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import porto.exam.dtos.CourseListDTO;
-import porto.exam.dtos.HttpResponseDTO;
-import porto.exam.exceptions.BadLogicException;
+import porto.exam.dtos.HttpResponseDto;
 import porto.exam.services.CourseService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/course")
@@ -21,20 +18,11 @@ public class CourseController {
     @Autowired
     private CourseService service;
 
-    @Operation(summary="get course + exam")
+    @Operation(summary="for HomeView; fetch course and its corresponding exam")
     @GetMapping
-    public ResponseEntity<HttpResponseDTO<?>> getByStudent( @RequestParam Integer studentId, @RequestParam String timezone) {
-        var result = service.getByStudent(studentId, timezone);
-        var httpRes = new HttpResponseDTO<List<CourseListDTO>>();
-        httpRes.setIsError(false);
-        httpRes.setMessage("");
-        httpRes.setData(result);
+    public ResponseEntity<HttpResponseDto<?>> fetchByStudent(@RequestParam String studentId, @Parameter(description = "IANA format") @RequestParam String timezone) {
+        var result = service.fetchByStudent(studentId, timezone);
+        var httpRes = new HttpResponseDto<>(false, "", result);
         return new ResponseEntity<>(httpRes, HttpStatus.OK);
-    }
-
-    //TODO delete later this is for testing error handling
-    @GetMapping("/error")
-    public ResponseEntity<HttpResponseDTO<?>> errorTest() throws Exception {
-        throw new BadLogicException("test bad logic error");
     }
 }
--- src/main/java/porto/exam/controllers/ExamController.java
+++ src/main/java/porto/exam/controllers/ExamController.java
@@ -1,13 +1,14 @@
 package porto.exam.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import porto.exam.dtos.ExamSubmitDTO;
-import porto.exam.dtos.ExamUpsertDTO;
-import porto.exam.dtos.HttpResponseDTO;
+import porto.exam.dtos.SubmitDto;
+import porto.exam.dtos.UpsertDto;
+import porto.exam.dtos.HttpResponseDto;
 import porto.exam.services.ExamService;
 
 @RestController
@@ -16,65 +17,53 @@ public class ExamController {
     @Autowired
     private ExamService service;
 
-    @Operation(summary="upsert exam")
+    @Operation(summary="for UpsertView; upsert exam")
     @PostMapping
-    public ResponseEntity<HttpResponseDTO<?>> upsert(@RequestBody ExamUpsertDTO dto) {
+    public ResponseEntity<HttpResponseDto<?>> upsert(@RequestBody UpsertDto dto) {
         service.upsert(dto);
-        var httpRes = new HttpResponseDTO<>();
-        httpRes.setIsError(false);
-        httpRes.setMessage("");
+        var httpRes = new HttpResponseDto<>(false, "", null);
         return new ResponseEntity<>(httpRes, HttpStatus.OK);
     }
 
-    @Operation(summary="get upsert exam data (for update prefill)")
+    @Operation(summary="for UpsertView; get prefill (update) data for form")
     @GetMapping("/data")
-    public ResponseEntity<HttpResponseDTO<?>> getUpsertExamData(@RequestParam Integer examId) {
-        var result = service.getUpsertExamData(examId);
-        var httpRes = new HttpResponseDTO<ExamUpsertDTO>();
-        httpRes.setIsError(false);
-        httpRes.setMessage("");
-        httpRes.setData(result);
+    public ResponseEntity<HttpResponseDto<?>> getUpsertData(@RequestParam String examId, @Parameter(description = "IANA format") @RequestParam String timezone) {
+        var result = service.fetchExamUpsertPrefillData(examId, timezone);
+        var httpRes = new HttpResponseDto<>(false, "", result);
         return new ResponseEntity<>(httpRes, HttpStatus.OK);
     }
 
-    @Operation(summary="submit/save answer of questions in exam")
+    @Operation(summary="for ExamView; submit (final)/save (temp) selected answer of question")
     @PostMapping("/answer")
-    public ResponseEntity<HttpResponseDTO<?>> submitAnswer(@RequestBody ExamSubmitDTO dto) {
+    public ResponseEntity<HttpResponseDto<?>> submitAnswer(@RequestBody SubmitDto dto) {
         service.submit(dto);
-        var httpRes = new HttpResponseDTO<>();
-        httpRes.setIsError(false);
-        httpRes.setMessage("");
+        var httpRes = new HttpResponseDto<>(false, "", null);
         return new ResponseEntity<>(httpRes, HttpStatus.OK);
     }
 
-    @Operation(summary="get exam data (for update prefill)")
+    @Operation(summary="for ExamView; get prefill (update) data for form")
     @GetMapping("/answer/data")
-    public ResponseEntity<HttpResponseDTO<?>> getExamData(@RequestParam Integer examId, @RequestParam Integer studentId) {
-        var result = service.getExamData(examId, studentId);
-        var httpRes = new HttpResponseDTO<>();
-        httpRes.setIsError(false);
-        httpRes.setMessage("");
-        httpRes.setData(result);
+    public ResponseEntity<HttpResponseDto<?>> getExamData(@RequestParam String examId, @RequestParam String studentId, @Parameter(description = "IANA format") @RequestParam String timezone) {
+        var result = service.fetchExamWithQnA(examId, studentId, timezone);
+        var httpRes = new HttpResponseDto<>(false, "", result);
         return new ResponseEntity<>(httpRes, HttpStatus.OK);
     }
 
-//    @GetMapping("/grade")
-//    public ResponseEntity<HttpResponseDTO<?>> grade(@RequestParam Integer examId, @RequestParam Integer courseTeacherId) {
-//        try {
-//            var result = service.grade(examId, courseTeacherId);
-//            var httpRes = new HttpResponseDTO<List<Integer>>();
-//            httpRes.setIsError(false);
-//            httpRes.setMessage("");
-//            httpRes.setData(result);
-//            return new ResponseEntity<>(httpRes, HttpStatus.OK);
-//        } catch (Exception e) {
-//            log.error("query failed | cause: {} | msg: {}", e.getCause(), e.getMessage());
-//            var httpRes = new HttpResponseDTO<String>();
-//            httpRes.setIsError(false);
-//            httpRes.setMessage("");
-//            httpRes.setData(e.getMessage());
-//            return new ResponseEntity<>(httpRes, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
+    @Operation(summary="for HomeView; grade exam")
+    @GetMapping("/grade")
+    public ResponseEntity<HttpResponseDto<?>> grade(@RequestParam String examId) {
+        service.grade(examId);
+        var httpRes = new HttpResponseDto<>(false, "", null);
+        return new ResponseEntity<>(httpRes, HttpStatus.OK);
+    }
+
+    @Operation(summary="for GradeView; fetch exam grade detail")
+    @GetMapping("/grade/data")
+    public ResponseEntity<HttpResponseDto<?>> fetchGrade(@RequestParam String courseTeacherId, @RequestParam String examId) {
+        service.grade(examId);
+        var result = service.fetchGradeData(courseTeacherId, examId);
+        var httpRes = new HttpResponseDto<>(false, "", result);
+        return new ResponseEntity<>(httpRes, HttpStatus.OK);
+    }
 }
 
