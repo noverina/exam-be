f09aad13ab2dd13ae329e5adb2752162da2a88a5
Noverina Sari <noverina.sari@indocyber.id>
2025-07-22 13:02:10 +0700

    fix https forwarding

--- src/main/java/porto/exam/ExamApplication.java
+++ src/main/java/porto/exam/ExamApplication.java
@@ -2,12 +2,23 @@ package porto.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class ExamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExamApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExamApplication.class, args);
-	}
-
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        var filter = new FilterRegistrationBean<>(new ForwardedHeaderFilter());
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return filter;
+    }
 }
+
+
